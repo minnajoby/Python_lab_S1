35af994b0308abc8ce06ef3e577e20cc070dad92
minnajoby <24mp2277@rit.ac.in>
2024-12-09 15:16:58 +0530

    Create Write a python script to display a) Current date and time b) Current Year c) Month of the year d) Week number of the year e) Weekday of the week f) Day of year g) Day of the month h) Day of week [ Use time and datetime Module] .py

---
+++ CYCLE 5/Program 2/Write a python script to display a) Current date and time b) Current Year c) Month of the year d) Week number of the year e) Weekday of the week f) Day of year g) Day of the month h) Day of week [ Use time and datetime Module] .py
@@ -0,0 +1,9 @@
+import datetime as m
+print("Current date and time:",m.datetime.now())
+print("Current year:",m.datetime.now().year)
+print("Month of the year:",m.datetime.now().strftime("%B"))
+print("Week number of the year:",m.datetime.now().strftime("%U"))
+print("Week day of the week:",m.datetime.now().strftime("%A"))
+print("Day of the year:",m.datetime.now().strftime("%d"))
+print("Day of the month:",m.datetime.now().day)
+print("Day of the year:",m.datetime.now().strftime("%A"))
